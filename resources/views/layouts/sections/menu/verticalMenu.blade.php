6d6908a1c2791a0a2b7fd52bffc1a43fb45964da
Abidhusain Seliya <seliyaabido2@gmail.com>
2026-03-26 17:25:22 +0530

    Initial commit

---
+++ resources/views/layouts/sections/menu/verticalMenu.blade.php
@@ -0,0 +1,185 @@
+<aside id="layout-menu" class="layout-menu menu-vertical menu">
+
+  <div class="app-brand demo">
+    <a href="/" class="app-brand-link">
+      <span class="app-brand-logo demo">@include('_partials.macros')</span>
+      <span class="app-brand-text demo menu-text fw-bold ms-3">Qamar HR</span>
+    </a>
+  </div>
+
+  <div class="menu-inner-shadow"></div>
+
+  <ul class="menu-inner py-1">
+
+    <!-- Dashboards -->
+    <!-- <li class="menu-item">
+      <a href="javascript:void(0);" class="menu-link menu-toggle">
+        <i class="menu-icon icon-base ti tabler-smart-home"></i>
+        <div>Dashboards</div>
+        <div class="badge bg-danger rounded-pill ms-auto">5</div>
+      </a>
+      <ul class="menu-sub">
+        <li class="menu-item"><a href="/" class="menu-link">Analytics</a></li>
+        <li class="menu-item"><a href="/dashboard/crm" class="menu-link">CRM</a></li>
+        <li class="menu-item"><a href="/app/ecommerce/dashboard" class="menu-link">eCommerce</a></li>
+        <li class="menu-item"><a href="/app/logistics/dashboard" class="menu-link">Logistics</a></li>
+        <li class="menu-item"><a href="/app/academy/dashboard" class="menu-link">Academy</a></li>
+      </ul>
+    </li> -->
+
+    <!-- Layouts -->
+    <!-- <li class="menu-item">
+      <a href="javascript:void(0);" class="menu-link menu-toggle">
+        <i class="menu-icon icon-base ti tabler-layout-sidebar"></i>
+        <div>Layouts</div>
+      </a>
+      <ul class="menu-sub">
+        <li class="menu-item"><a href="/layouts/collapsed-menu" class="menu-link">Collapsed menu</a></li>
+        <li class="menu-item"><a href="/layouts/content-navbar" class="menu-link">Content navbar</a></li>
+        <li class="menu-item"><a href="/layouts/content-nav-sidebar" class="menu-link">Content nav + Sidebar</a></li>
+        <li class="menu-item"><a href="/layouts/horizontal" target="_blank" class="menu-link">Horizontal</a></li>
+        <li class="menu-item"><a href="/layouts/without-menu" class="menu-link">Without menu</a></li>
+        <li class="menu-item"><a href="/layouts/without-navbar" class="menu-link">Without navbar</a></li>
+        <li class="menu-item"><a href="/layouts/fluid" class="menu-link">Fluid</a></li>
+        <li class="menu-item"><a href="/layouts/container" class="menu-link">Container</a></li>
+        <li class="menu-item"><a href="/layouts/blank" target="_blank" class="menu-link">Blank</a></li>
+      </ul>
+    </li> -->
+
+    <!-- Front Pages -->
+    <!-- <li class="menu-item">
+      <a href="javascript:void(0);" class="menu-link menu-toggle">
+        <i class="menu-icon icon-base ti tabler-files"></i>
+        <div>Front Pages</div>
+      </a>
+      <ul class="menu-sub">
+        <li class="menu-item"><a href="/front-pages/landing" target="_blank" class="menu-link">Landing</a></li>
+        <li class="menu-item"><a href="/front-pages/pricing" target="_blank" class="menu-link">Pricing</a></li>
+        <li class="menu-item"><a href="/front-pages/payment" target="_blank" class="menu-link">Payment</a></li>
+        <li class="menu-item"><a href="/front-pages/checkout" target="_blank" class="menu-link">Checkout</a></li>
+        <li class="menu-item"><a href="/front-pages/help-center" target="_blank" class="menu-link">Help Center</a></li>
+      </ul>
+    </li> -->
+
+    <!-- Laravel Example -->
+    <!-- <li class="menu-item">
+      <a href="javascript:void(0);" class="menu-link menu-toggle">
+        <i class="menu-icon icon-base ti tabler-brand-php"></i>
+        <div>Laravel Example</div>
+      </a>
+      <ul class="menu-sub">
+        <li class="menu-item"><a href="/laravel/user-management" class="menu-link">User Management</a></li>
+      </ul>
+    </li> -->
+
+    <!-- Header -->
+    <!-- <li class="menu-header small"><span class="menu-header-text">Apps & Pages</span></li> -->
+
+    <!-- Apps -->
+    <!-- <li class="menu-item"><a href="/app/email" class="menu-link"><i class="menu-icon ti tabler-mail"></i><div>Email</div></a></li>
+    <li class="menu-item"><a href="/app/chat" class="menu-link"><i class="menu-icon ti tabler-messages"></i><div>Chat</div></a></li>
+    <li class="menu-item"><a href="/app/calendar" class="menu-link"><i class="menu-icon ti tabler-calendar"></i><div>Calendar</div></a></li>
+    <li class="menu-item"><a href="/app/kanban" class="menu-link"><i class="menu-icon ti tabler-layout-kanban"></i><div>Kanban</div></a></li> -->
+
+    <!-- eCommerce FULL -->
+    <!-- <li class="menu-item">
+      <a href="javascript:void(0);" class="menu-link menu-toggle">
+        <i class="menu-icon ti tabler-shopping-cart"></i>
+        <div>eCommerce</div>
+      </a>
+
+      <ul class="menu-sub">
+        <li class="menu-item"><a href="/app/ecommerce/dashboard" class="menu-link">Dashboard</a></li>
+
+        <li class="menu-item">
+          <a href="javascript:void(0);" class="menu-link menu-toggle">Products</a>
+          <ul class="menu-sub">
+            <li class="menu-item"><a href="/app/ecommerce/product/list" class="menu-link">Product List</a></li>
+            <li class="menu-item"><a href="/app/ecommerce/product/add" class="menu-link">Add Product</a></li>
+            <li class="menu-item"><a href="/app/ecommerce/product/category" class="menu-link">Category List</a></li>
+          </ul>
+        </li>
+
+        <li class="menu-item">
+          <a href="javascript:void(0);" class="menu-link menu-toggle">Order</a>
+          <ul class="menu-sub">
+            <li class="menu-item"><a href="/app/ecommerce/order/list" class="menu-link">Order List</a></li>
+            <li class="menu-item"><a href="/app/ecommerce/order/details" class="menu-link">Order Details</a></li>
+          </ul>
+        </li>
+
+        <li class="menu-item">
+          <a href="javascript:void(0);" class="menu-link menu-toggle">Customer</a>
+          <ul class="menu-sub">
+            <li class="menu-item"><a href="/app/ecommerce/customer/all" class="menu-link">All Customers</a></li>
+
+            <li class="menu-item">
+              <a href="javascript:void(0);" class="menu-link menu-toggle">Customer Details</a>
+              <ul class="menu-sub">
+                <li class="menu-item"><a href="/app/ecommerce/customer/details/overview" class="menu-link">Overview</a></li>
+                <li class="menu-item"><a href="/app/ecommerce/customer/details/security" class="menu-link">Security</a></li>
+                <li class="menu-item"><a href="/app/ecommerce/customer/details/billing" class="menu-link">Address & Billing</a></li>
+                <li class="menu-item"><a href="/app/ecommerce/customer/details/notifications" class="menu-link">Notifications</a></li>
+              </ul>
+            </li>
+
+          </ul>
+        </li>
+
+        <li class="menu-item"><a href="/app/ecommerce/manage/reviews" class="menu-link">Manage Reviews</a></li>
+        <li class="menu-item"><a href="/app/ecommerce/referrals" class="menu-link">Referrals</a></li>
+
+        <li class="menu-item">
+          <a href="javascript:void(0);" class="menu-link menu-toggle">Settings</a>
+          <ul class="menu-sub">
+            <li class="menu-item"><a href="/app/ecommerce/settings/details" class="menu-link">Store Details</a></li>
+            <li class="menu-item"><a href="/app/ecommerce/settings/payments" class="menu-link">Payments</a></li>
+            <li class="menu-item"><a href="/app/ecommerce/settings/checkout" class="menu-link">Checkout</a></li>
+            <li class="menu-item"><a href="/app/ecommerce/settings/shipping" class="menu-link">Shipping & Delivery</a></li>
+            <li class="menu-item"><a href="/app/ecommerce/settings/locations" class="menu-link">Locations</a></li>
+            <li class="menu-item"><a href="/app/ecommerce/settings/notifications" class="menu-link">Notifications</a></li>
+          </ul>
+        </li>
+
+      </ul>
+    </li> -->
+
+    <!-- (Rest SAME pattern continues exactly...) -->
+
+    <!-- Multi Level -->
+    <!-- <li class="menu-item">
+      <a href="javascript:void(0);" class="menu-link menu-toggle">
+        <i class="menu-icon ti tabler-layout-board"></i>
+        <div>Multi Level</div>
+      </a>
+      <ul class="menu-sub">
+        <li class="menu-item">
+          <a href="javascript:void(0);" class="menu-link menu-toggle">Level 2</a>
+          <ul class="menu-sub">
+            <li class="menu-item">
+              <a href="javascript:void(0);" class="menu-link">Level 3</a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </li> -->
+
+    <!-- Support -->
+    <!-- <li class="menu-item">
+      <a href="https://pixinvent.ticksy.com/" target="_blank" class="menu-link">
+        <i class="menu-icon ti tabler-lifebuoy"></i>
+        <div>Support</div>
+      </a>
+    </li> -->
+
+    <!-- Docs -->
+    <!-- <li class="menu-item">
+      <a href="https://demos.pixinvent.com/vuexy-html-admin-template/documentation/laravel-introduction.html"
+         target="_blank" class="menu-link">
+        <i class="menu-icon ti tabler-file-description"></i>
+        <div>Documentation</div>
+      </a>
+    </li> -->
+
+  </ul>
+</aside>
